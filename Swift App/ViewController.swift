58560645648fd860f6398119a6972a949b7cd236
Ben Leimbach <benleimbach@gmail.com>
2020-07-29 07:31:16 -0600

    Boolean fun

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -8,11 +8,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var theLabel: UILabel!
     
     @IBOutlet weak var text1: UITextField!
-
+    
     
     
     @IBOutlet weak var text2: UITextField!
@@ -20,18 +20,26 @@ class ViewController: UIViewController {
     
     @IBAction func buttonTapped(_ sender: Any) {
         
-        theLabel.text = "Answer: \(Double(text1.text!)! + Double(text2.text!)!)"
+        let addition = false
+        
+        if addition {
+            theLabel.text = "Answer: \(Double(text1.text!)! + Double(text2.text!)!)"
+        } else {
+            theLabel.text = "Answer: \(Double(text1.text!)! - Double(text2.text!)!)"
+            
+        }
         
         }
         
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            // Do any additional setup after loading the view.
+            
+            
+        }
         
         
     }
-
-
-}
 
