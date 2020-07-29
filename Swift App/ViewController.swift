c10dbb1c026be01e8d4b83f43f8e1e6fcdcffc11
Ben Leimbach <benleimbach@gmail.com>
2020-07-28 20:38:01 -0600

    My first commit

---
+++ Swift App/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Swift App
+//
+//  Created by Ben Leimbach on 7/27/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var theLabel: UILabel!
+    
+    var tapCount = 0
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        
+        tapCount = tapCount + 1
+   
+        if tapCount >= 10 {
+            theLabel.text = "You tapped the button 10 times!"
+        }
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+    }
+
+
+}
+
